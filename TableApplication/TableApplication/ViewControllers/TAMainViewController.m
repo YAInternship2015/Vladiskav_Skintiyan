2ac98fefcffdae5157fe1f31faf99507f1286efa
Vladislav Skintiyan <vs@gamayun.sk>
2015-08-13 08:44:37 +0300

    Fixed model usage

--- TableApplication/TableApplication/ViewControllers/TAMainViewController.m
+++ TableApplication/TableApplication/ViewControllers/TAMainViewController.m
@@ -14,7 +14,7 @@ const uint8_t kRowCount = 10;
 
 @interface TAMainViewController ()
 
-@property (strong, nonatomic) TADataModel *dataModel;
+@property (strong, nonatomic) NSMutableArray *model;
 
 @end
 
@@ -22,26 +22,30 @@ const uint8_t kRowCount = 10;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.model = [NSMutableArray array];
     
-    self.dataModel = [TADataModel defaultModel];
+    for (int i = 0; i < kRowCount; ++i) {
+        [self.model addObject:[[TADataModel defaultModel] getRandomObject]];
+    }
 }
 
-#pragma mark - Table view data source
+#pragma mark - UITableViewDataSource
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return kRowCount;
+    return [self.model count];
 }
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     TATableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
     
-    cell.titleLabel.text = [self.dataModel getRandomObject];
+    cell.titleLabel.text = [self.model objectAtIndex:indexPath.row];
     cell.cellImageView.image = [UIImage imageNamed:@"design_icon"];
     
     return cell;
 }
 
+#pragma mark - UITableViewDelegate
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     return 80.0;
 }
